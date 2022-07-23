6ef0351b9b959f767a6d35dc96868aaf268b8601
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-07-23 10:05:11 +0900

    refactor: Hard-coated strings are now constants.

--- src/google_api/const.py
+++ src/google_api/const.py
@@ -12,3 +12,13 @@ TO_ADDRESS = os.environ.get('TO_ADDRESS')
 
 # SpreadSheet
 OPEN_BY_KEY = os.environ.get('OPEN_BY_KEY')
+
+# Zip
+ZIP_DIR = 'zip_storage'
+ZIP_NAME = 'sample.zip'
+
+# Token Key
+CREDENTIALS = 'token/credentials.json'
+SECRET_CREDENTIALS_JSON_OATH = 'token/gmail-api-355022-service-account-key.json'
+TOKEN_JSON = 'token/token.json'
+TOKEN_PICKLE = 'token/token.pickle'
