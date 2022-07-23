9f4f8334c77b939642c91bd56927e5f65a90059e
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-07-23 19:18:24 +0900

    refactor: Adjusted to use isnumeric() to handle non-numeric user-selected branches

--- src/google_api/views/send_gmail_view.py
+++ src/google_api/views/send_gmail_view.py
@@ -17,26 +17,39 @@ def send_gmail_select_user_console(users: list) -> int:
         print(f"{idx + 1}: {user['account_name']}")
 
     while True:
-        user_id = input('Please select the processing user by number\n')
-        logger.info({
-            'msg': 'Selected UserID',
-            'user_id': user_id,
-        })
         try:
-            user_id = int(user_id)
-        except ValueError as _:
-            msg = 'Please select by number\n'
-            print(msg)
+            user_id = input('Please select the processing user by number\n')
             logger.info({
-                'msg': 'msg',
+                'msg': 'Selected UserID',
                 'user_id': user_id,
             })
-            continue
-        if user_id > len(users) or user_id < 1:
-            print('Out of selection, please select again')
-            continue
-        break
+            if not user_id.isnumeric():
+                msg = 'Please select by number\n'
+                print(msg)
+                logger.info({
+                    'msg': msg,
+                    'user_id': user_id,
+                })
+                continue
+            user_id = int(user_id)
+            if user_id > len(users) or user_id < 1:
+                msg = 'Out of selection, please select again\n'
+                print(msg)
+                logger.info({
+                    'msg': msg,
+                    'user_id': user_id,
+                })
+                continue
+            break
+
+        except Exception as ex:
+            # TODO: Add Slack Alert
+            logger.error({
+                'msg': 'An unexpected exception occurred. Processing will be stopped.',
+                'ex': ex,
+            })
+            raise Exception
 
     logger.info({'msg': 'End Select users to be processed'})
-    return user_id
-
+    # starting number 0. Adjustment of the acquisition number of the spreadsheet
+    return user_id - 1
