ab1bf92a15c5b4964044e9ad41b055b3539d78a0
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-08-14 09:15:24 +0900

    run test

--- src/google_api/main.py
+++ src/google_api/main.py
@@ -1,4 +1,17 @@
 from controllers import gmail_controller
+from models import gmail_api
 
 if __name__ == '__main__':
-    gmail_controller.send_gmail_attach_file()
+    # gmail_controller.send_gmail_attach_file()
+    ga = gmail_api.GmailApi()
+    receives = ga.receive_gmail(3, query='test aaa bbb')
+
+    if receives:
+        for receive in receives:
+            print(receive['Received'])
+            print(receive['Delivered-To'])
+            print(receive['snippet'])
+            print('-' * 20)
+    else:
+        print('msg: No search results')
+    # ga.receive_gmail_threads()
