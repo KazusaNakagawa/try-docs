0086e8e581d4a56e5fb239d4dee96c0198f1f057
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-08-15 08:59:25 +0900

    feat: test main run receive Gmail

--- src/google_api/main.py
+++ src/google_api/main.py
@@ -4,13 +4,15 @@ from models import gmail_api
 if __name__ == '__main__':
     # gmail_controller.send_gmail_attach_file()
     ga = gmail_api.GmailApi()
-    receives = ga.receive_gmail(3, query='test aaa bbb')
+    receives = ga.receive_gmail(10, query='')
 
     if receives:
         for receive in receives:
-            print(receive['Received'])
-            print(receive['Delivered-To'])
-            print(receive['snippet'])
+            print('jst_time: ', receive['jst_time'])
+            print('From: ', receive['From'])
+            print('To: ', receive['To'])
+            print('Subject: ', receive['Subject'])
+            print('snippet: ', receive['snippet'][0:50])
             print('-' * 20)
     else:
         print('msg: No search results')
